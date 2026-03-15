7580b592e738c0d4d03bc1aa4080c454432d4a77
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-15 15:54:32 +0100

    created a node server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log('Hello Node!');
+import http from "node:http";
+
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server!");
+});
+
+server.listen(PORT, () => console.log(`server is running on ${PORT}`));
