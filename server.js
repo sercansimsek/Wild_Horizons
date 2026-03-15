c619641bdb8de59bcee7db909de65c19d762c682
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-15 19:40:18 +0100

    Path parameters added

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ const server = http.createServer(async (req, res) => {
 		res.setHeader("Content-Type", "application/json");
 		res.statusCode = 200;
 		res.end(JSON.stringify(destinations));
+	} else if (req.url.startsWith("/api/continent") && req.method === "GET") {
+		const continent = req.url.split("/").pop();
+		const filteredData = destinations.filter((destination) => {
+			return destination.continent.toLowerCase() === continent.toLowerCase();
+		});
+		res.setHeader("Content-Type", "application/json");
+		res.statusCode = 200;
+		res.end(JSON.stringify(filteredData));
 	} else {
 		res.setHeader("Content-Type", "application/json");
 		res.statusCode = 404;
