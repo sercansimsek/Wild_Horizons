59f5240d29a6a2245268d3049768a8299241c0fa
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-15 19:26:27 +0100

    Route not found

--- server.js
+++ server.js
@@ -4,12 +4,22 @@ import { getDataFromDB } from "./database/db.js";
 const PORT = 8000;
 
 const server = http.createServer(async (req, res) => {
-  const destinations = await getDataFromDB();
+	const destinations = await getDataFromDB();
 
-  if (req.url === "/api" && req.method === "GET") {
-    res.write(JSON.stringify(destinations));
-    res.end();
-  }
+	if (req.url === "/api" && req.method === "GET") {
+		res.setHeader("Content-Type", "application/json");
+		res.statusCode = 200;
+		res.end(JSON.stringify(destinations));
+	} else {
+		res.setHeader("Content-Type", "application/json");
+		res.statusCode = 404;
+		res.end(
+			JSON.stringify({
+				error: "not found",
+				message: "The requested route does not exist",
+			}),
+		);
+	}
 });
 
 server.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
