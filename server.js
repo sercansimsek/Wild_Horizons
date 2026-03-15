1a20aef262aded0e94e9eeea24efebdebf5df1f4
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-15 16:47:19 +0100

    accessed the data

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import http from "node:http";
+import { getDataFromDB } from "./database/db.js";
 
 const PORT = 8000;
 
-const server = http.createServer((req, res) => {
-  res.end("Hello from the server!");
+const server = http.createServer(async (req, res) => {
+  const destinations = await getDataFromDB();
+
+  if (req.url === "/api" && req.method === "GET") {
+    res.write(JSON.stringify(destinations));
+    res.end();
+  }
 });
 
-server.listen(PORT, () => console.log(`server is running on ${PORT}`));
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
