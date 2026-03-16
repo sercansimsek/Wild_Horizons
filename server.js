20a4620af5c789f8b73ee0ce53ac6fe0dffa5408
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-16 15:08:03 +0100

    More query options to filter

--- server.js
+++ server.js
@@ -2,39 +2,39 @@ import http from "node:http";
 import { getDataFromDB } from "./database/db.js";
 import { sendJSONResponse } from "./utils/sendJSONResponse.js";
 import { getDataByPathParams } from "./utils/getDataByPathParams.js";
+import { getDataByQueryParams } from "./utils/getDataByQueryParams.js";
 
 const PORT = 8000;
 
 const server = http.createServer(async (req, res) => {
-	const destinations = await getDataFromDB();
+  const destinations = await getDataFromDB();
 
-	const urlObj = new URL(req.url, `http://${req.headers.host}`);
+  const urlObj = new URL(req.url, `http://${req.headers.host}`);
 
-	const queryObj = Object.fromEntries(urlObj.searchParams);
+  const queryObj = Object.fromEntries(urlObj.searchParams);
 
-	console.log(queryObj);
-
-	if (req.url === "/api" && req.method === "GET") {
-		sendJSONResponse(res, 200, destinations);
-	} else if (req.url.startsWith("/api/continent") && req.method === "GET") {
-		const continent = req.url.split("/").pop();
-		const filteredData = getDataByPathParams(
-			destinations,
-			"continent",
-			continent,
-		);
-		sendJSONResponse(res, 200, filteredData);
-	} else if (req.url.startsWith("/api/country") && req.method === "GET") {
-		const country = req.url.split("/").pop();
-		const filteredData = getDataByPathParams(destinations, "country", country);
-		sendJSONResponse(res, 200, filteredData);
-	} else {
-		res.setHeader("Content-Type", "application/json");
-		sendJSONResponse(res, 404, {
-			error: "not found",
-			message: "The requested route does not exist",
-		});
-	}
+  if (urlObj.pathname === "/api" && req.method === "GET") {
+    let filteredData = getDataByQueryParams(destinations, queryObj);
+    sendJSONResponse(res, 200, filteredData);
+  } else if (req.url.startsWith("/api/continent") && req.method === "GET") {
+    const continent = req.url.split("/").pop();
+    const filteredData = getDataByPathParams(
+      destinations,
+      "continent",
+      continent,
+    );
+    sendJSONResponse(res, 200, filteredData);
+  } else if (req.url.startsWith("/api/country") && req.method === "GET") {
+    const country = req.url.split("/").pop();
+    const filteredData = getDataByPathParams(destinations, "country", country);
+    sendJSONResponse(res, 200, filteredData);
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    sendJSONResponse(res, 404, {
+      error: "not found",
+      message: "The requested route does not exist",
+    });
+  }
 });
 
 server.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
