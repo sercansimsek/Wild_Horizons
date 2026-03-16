b5a93a429f9cd9255adf433740928d96f84206d4
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-16 10:59:02 +0100

    DRY code practice

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import http from "node:http";
 import { getDataFromDB } from "./database/db.js";
+import { sendJSONResponse } from "./utils/sendJSONResponse.js";
 
 const PORT = 8000;
 
@@ -7,26 +8,19 @@ const server = http.createServer(async (req, res) => {
 	const destinations = await getDataFromDB();
 
 	if (req.url === "/api" && req.method === "GET") {
-		res.setHeader("Content-Type", "application/json");
-		res.statusCode = 200;
-		res.end(JSON.stringify(destinations));
+		sendJSONResponse(res, 200, destinations);
 	} else if (req.url.startsWith("/api/continent") && req.method === "GET") {
 		const continent = req.url.split("/").pop();
 		const filteredData = destinations.filter((destination) => {
 			return destination.continent.toLowerCase() === continent.toLowerCase();
 		});
-		res.setHeader("Content-Type", "application/json");
-		res.statusCode = 200;
-		res.end(JSON.stringify(filteredData));
+		sendJSONResponse(res, 200, filteredData);
 	} else {
 		res.setHeader("Content-Type", "application/json");
-		res.statusCode = 404;
-		res.end(
-			JSON.stringify({
-				error: "not found",
-				message: "The requested route does not exist",
-			}),
-		);
+		sendJSONResponse(res, 404, {
+			error: "not found",
+			message: "The requested route does not exist",
+		});
 	}
 });
 
